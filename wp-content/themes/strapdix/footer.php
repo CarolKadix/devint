0bc844ffb4f9998124e48ae480bea03a3b88a4e4
Alex Koti <alexkoti@gmail.com>
2015-03-12 01:11:31 -0300

    Correção na chamada das imagens do rodapé

--- wp-content/themes/strapdix/footer.php
+++ wp-content/themes/strapdix/footer.php
@@ -12,12 +12,12 @@
                                
             </div>  
                              
-            <div id="social-icons-container" class="col-md-3 right no-padding">              
+            <div id="social-icons-container" class="col-md-3 right no-padding">
                 <ul>               
-                <!--rss-->      <li><?php echo '<a class="btn btn-block btn-social btn-rss"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/icon-feed.png">'. $strapdix_options['rss_uid'] . '</a>'; ?> </li>              
-                <!--twitter-->  <li><?php echo '<a class="btn btn-block btn-social btn-twitter"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/icon-twitter.png">'. $strapdix_options['twitter_uid'] . '</a>'; ?> </li>               
-                <!--facebook--> <li><?php echo '<a class="btn btn-block btn-social btn-facebook"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/icon-facebook.png">'. $strapdix_options['facebook_uid'] . '</a>'; ?> </li>
-                <!--contato-->  <li><?php echo '<a class="btn btn-block btn-social btn-envelope"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/icon-email.png">'. $strapdix_options['mail_uid'] . '</a>'; ?> </li>
+                <!--rss-->      <li><?php echo '<a class="btn btn-block btn-social btn-rss">     <img src="' . get_stylesheet_directory_uri() . '/img/icon-feed.png">'     . $strapdix_options['rss_uid']      . '</a>'; ?></li>
+                <!--twitter-->  <li><?php echo '<a class="btn btn-block btn-social btn-twitter"> <img src="' . get_stylesheet_directory_uri() . '/img/icon-twitter.png">'  . $strapdix_options['twitter_uid']  . '</a>'; ?></li>
+                <!--facebook--> <li><?php echo '<a class="btn btn-block btn-social btn-facebook"><img src="' . get_stylesheet_directory_uri() . '/img/icon-facebook.png">' . $strapdix_options['facebook_uid'] . '</a>'; ?></li>
+                <!--contato-->  <li><?php echo '<a class="btn btn-block btn-social btn-envelope"><img src="' . get_stylesheet_directory_uri() . '/img/icon-email.png">'    . $strapdix_options['mail_uid']     . '</a>'; ?></li>
                 </ul>              
             </div>
           </div>      
